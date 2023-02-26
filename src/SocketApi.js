c183f2c3a5150a70fe148b4d8fcd85cc2527c99d
Koray Can Yilmaz <krycnylmz@gmail.com>
2023-02-26 11:04:23 +0300

    Create SocketApi.js

---
+++ src/SocketApi.js
@@ -0,0 +1,24 @@
+import { io } from "socket.io-client";
+
+let socket;
+
+export const init = () => {
+  console.log("Connecting...");
+  socket = io("http://192.168.1.100:3001", { transports: ["websocket"] });
+
+  socket.on("connect", () => {
+    console.log("Connected");
+  });
+};
+
+export const sendMessage = (message) => {
+  if (socket) socket.emit("new-message", message);
+};
+
+export const subscribeChat = (cb) => {
+  if (!socket) return;
+  socket.on("receive-message", (message) => {
+    console.log("yeni bir mesaj var", message);
+    cb(message);
+  });
+};
